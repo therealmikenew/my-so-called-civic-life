7977af484e0bf48911c71a88a53ab567a7dd2842
Michael Newman <therealmichaelnewman@gmail.com>
2022-02-14 09:27:28 -0500

    update CivicLeaders.vue

--- my-so-called-civic-life/src/components/CivicLeaders.vue
+++ my-so-called-civic-life/src/components/CivicLeaders.vue
@@ -1,68 +1,126 @@
 <template>
 <div>
-    <h1>Civic Leaders</h1>
+    <h1>My Civic Leaders</h1>
     <h2 >My Federal Representative</h2>
-    <h3 @click='selectRep(fedRepOfficials.name)'>{{fedRepOfficials.name}}</h3>
-
-
-    <div v-if='fedRepOfficials.photoUrl'>
-        <img :src="fedRepOfficials.photoUrl">
-    </div>
-
-    
-    <h4>{{fedRepOfficials.party}}</h4>
-        <div v-for='url in fedRepUrls' :key='url.id'>
-               <a v-bind:href='url' target="_blank">{{url}}</a>
-        </div>
+    <vs-card @click='selectRep(fedRepOfficials.name)' type="3">
+        <template #title>
+            <h3 >{{fedRepOfficials.name}}</h3>
+        </template>
+        <template #img>
+            <div v-if='fedRepOfficials.photoUrl'>
+                <img :src="fedRepOfficials.photoUrl">
+            </div>
+        </template>
+        <template #text>
+            <h4>{{fedRepOfficials.party}}</h4>
+            <p v-for='url in fedRepUrls' :key='url.id'><a v-bind:href='url' target="_blank">{{url}}</a></p>
+        </template>
+   
+    </vs-card>
 
     <h2>My Federal Senators</h2>
     <div v-for='fedSen in fedSenOfficials' :key='fedSen.id'>
-        <h3 @click="selectSen(fedSen.name)">{{fedSen.name}}</h3>
-            <img :src="fedSen.photoUrl" alt="no photo">
-
-        <h4>{{fedSen.party}}</h4>
-        <div v-for='url in fedSen.urls' :key='url.id'>
+        <vs-card @click="selectSen(fedSen.name)" type="3">
+            <template #title>
+                <h3 >{{fedSen.name}}</h3>
+            </template>
+            <template #img>
+                <img :src="fedSen.photoUrl" alt="no photo">
+            </template>
+            <template #text>
+                <p>{{fedSen.party}}</p>
+                <p v-for='url in fedSen.urls' :key='url.id'>
             
             <a v-bind:href='url' target="_blank">{{url}}</a>
-        </div>
+        </p>
+      
+    </template>
+    
+    
+  </vs-card>
+        
+           
     </div>
 
-    <h2>My State Senator</h2>
-    <h3>{{stateSen.name}}</h3>
-
-
+<h2>My State Senator</h2>
+      <vs-card type="3">
+    <template #title>
+      <h3>{{stateSen.name}}</h3>
+    </template>
+    <template #img>
       <div v-if='stateSen.photoUrl'>
          <img :src="stateSen.photoUrl" >
     </div>
-   
-    <h4>{{stateSen.party}}</h4>
-        <div v-for='url in stateSen.urls' :key='url.id'>
+    </template>
+    <template #text>
+      <p>
+{{stateSen.party}}     </p>
+<div v-for='url in stateSen.urls' :key='url.id'>
              <a v-bind:href='url' target="_blank">{{url}}</a>
         </div>
+    </template>
+   
+  </vs-card>
 
-    <h2>My State Representative</h2>
-    <h3>{{stateRep.name}}</h3>
-
-     <div v-if='stateRep.photoUrl'>
+<h2>My State Representative</h2>
+  <vs-card type="3">
+    <template #title>
+      <h3>{{stateRep.name}}</h3>
+    </template>
+    <template #img>
+      <div v-if='stateRep.photoUrl'>
          <img :src="stateRep.photoUrl" alt='photo' >
     </div>
-
-    
-   
-    <h4>{{stateRep.party}}</h4>
+    </template>
+    <template #text>
+      <p>{{stateRep.party}}</p>
         <div v-for='url in stateRep.urls' :key='url.id'>
                <a v-bind:href='url' target="_blank">{{url}}</a>
         </div>
+    </template>
+    
+  </vs-card>
 
 
-    <h2>My City Council Member(s)</h2>
+ <h2>My City Council Member(s)</h2>
+
+
+
     <div v-for="city in cityRep" :key='city.id'>
-        <h3>{{city.name}}</h3>
-        <h4>{{city.party}}</h4>
-        <div v-for='url in city.urls' :key='url.id'>
+
+        <vs-card type="3">
+    <template #title>
+      <h3>{{city.name}}</h3>
+      
+    </template>
+    <template #img>
+      
+    </template>
+    <template #text>
+      <p>{{city.party}}</p>
+      <div v-for='url in city.urls' :key='url.id'>
              <a v-bind:href='url' target="_blank">{{url}}</a>
         </div>
+    </template>
+   
+  </vs-card>
+
+
+
+
+
+
+
+        
+        
+        
     </div>
+
+          
+
+
+
+   
 </div>
 </template>
 
@@ -138,8 +196,8 @@ export default {
 
 
 <style >
-img {
-    width: 200px;
+.img {
+    width: 150px;
 }
 
 </style>
